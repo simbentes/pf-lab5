102a7681a317d436248bf368f98ce810676a014f
Simão Bentes <sdgbentes@gmail.com>
2022-01-04 11:16:03 +0000

    react-router

--- src/NavBar.js
+++ src/NavBar.js
@@ -190,7 +190,12 @@ function NavBar() {
                 </>
               )}
             </Popover>
-
+            <a
+              href='#'
+              className='text-base font-medium text-gray-500 hover:text-gray-900'
+            >
+              Últimas
+            </a>
             <a
               href='#'
               className='text-base font-medium text-gray-500 hover:text-gray-900'
