424bc759024b2b89d21804aeec2f5c9516c91876
Simão Bentes <sdgbentes@gmail.com>
2022-01-10 21:11:56 +0000

    noticia miniaturas

--- src/NoticiaMiniatura.js
+++ src/NoticiaMiniatura.js
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 function NoticiaMiniatura(props) {
+  const navegar = useNavigate();
   return (
     <div className='bg-white shadow-md m-3 rounded-md'>
       <div key={props.info.id}>
-        <img src={props.info.multimediaPrincipal} className='mx-auto w-full' />{" "}
+        <img
+          src={props.info.multimediaPrincipal}
+          className='mx-auto w-full hover:cursor-pointer'
+          onClick={() => navegar("/noticia/" + props.info.id)}
+        />
         <div className='p-2'>
           <h6 className='text-base leading-4 font-semibold'>
             {props.info.titulo}
