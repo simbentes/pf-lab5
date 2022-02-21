a6842f4c18a019a89531cb555611d1bde0332b95
Simão Bentes <sdgbentes@gmail.com>
2022-02-21 17:43:02 +0000

    fix loading

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import { PlayIcon, PauseIcon } from "@heroicons/react/solid";
+import { PlayIcon, PauseIcon, RefreshIcon } from "@heroicons/react/solid";
 
 const PlayButton = React.forwardRef((props, ref) => {
-
   let playjsx = <PlayIcon className='h-10 w-10 fill-slate-800' aria-hidden='true' />;
   let pausejsx = <PauseIcon className='h-10 w-10 fill-slate-800' aria-hidden='true' />;
-  let loadingjsx = <p>loading</p>
+  let loadingjsx = "Loading";
 
   let reference = useRef({ hasStarted: false, times: 0 });
   const [disable, setDisable] = useState(true);
+  const [loading, setLoading] = useState("animate-pulse");
   const [buttonContent, setButtonContent] = useState(loadingjsx);
-
-  
 
   useEffect(() => {
     if (ref != null) {
-      ref.current.pause_func = pause
-      ref.current.refresh_func = refresh
+      ref.current.pause_func = pause;
+      ref.current.refresh_func = refresh;
     }
-
   });
 
   useEffect(() => {
@@ -28,17 +25,15 @@ const PlayButton = React.forwardRef((props, ref) => {
     //console.log("vel: ", typeof props.def_audio.vel, "pitch: ", typeof props.def_audio.pitch);
     reference.current.times += 1;
 
-    fetch_audio()
-    
+    fetch_audio();
   }, [props.contents]);
 
   const fetch_audio = () => {
-
     if (props.contents[0] == null) {
-      setButtonContent(<p>Sem audio</p>)
-      return 
+      setButtonContent("Sem audio");
+      setLoading("");
+      return;
     }
-
 
     reference.current.context = new AudioContext();
     reference.current.source = reference.current.context.createBufferSource();
@@ -60,10 +55,9 @@ const PlayButton = React.forwardRef((props, ref) => {
       .then((response) => response.arrayBuffer())
       .then((arrayBuffer) => {
         try {
-          return reference.current.context.decodeAudioData(arrayBuffer)
-        }
-        catch (e){
-           //console.log(e)
+          return reference.current.context.decodeAudioData(arrayBuffer);
+        } catch (e) {
+          //console.log(e)
         }
       })
       .then((audioBuffer) => {
@@ -75,53 +69,58 @@ const PlayButton = React.forwardRef((props, ref) => {
         reference.current.source.connect(reference.current.context.destination);
         setDisable(false);
         setButtonContent(playjsx);
+        setLoading("");
       });
-  }
+  };
 
   const pause = () => {
     if (disable || !reference.current.hasStarted) return;
     reference.current.context.suspend().then();
     setButtonContent(playjsx);
+    setLoading("");
   };
 
   const refresh = () => {
-    console.log("yo")
-    pause()
-    reference.current.hasStarted = false
-    setDisable(true)
+    console.log("yo");
+    pause();
+    reference.current.hasStarted = false;
+    setDisable(true);
     setButtonContent(loadingjsx);
-    fetch_audio()
-  }
+    setLoading("animation-pulse");
+    fetch_audio();
+  };
 
   const play_pause = () => {
     if (disable) return;
 
     if (props.pauseAllFunc != undefined) props.pauseAllFunc();
 
-    console.log(ref.current.state)
+    console.log(ref.current.state);
 
     if (!reference.current.hasStarted) {
       reference.current.source.start();
       reference.current.hasStarted = true;
       setButtonContent(pausejsx);
-      ref.current.state = "running"
+      ref.current.state = "running";
     } else if (ref.current.state == "running" || reference.current.context.state == "running") {
       reference.current.context.suspend().then();
-      ref.current.state = "suspended"
-      setButtonContent(playjsx);      
+      ref.current.state = "suspended";
+      setButtonContent(playjsx);
     } else if (ref.current.state == "suspended" || reference.current.context.state == "suspended") {
       reference.current.context.resume().then();
-      ref.current.state = "running"
+      ref.current.state = "running";
       setButtonContent(pausejsx);
     }
   };
 
   return (
-    <div ref={ref} className='my-3 grid items-center rounded-lg bg-slate-200 w-full h-12 px-1'>
+    <div ref={ref} className={`my-3 flex justify-between items-center rounded-lg bg-slate-200 w-full h-12 px-1 ${loading}`}>
       <div onClick={play_pause} className='cursor-pointer'>
         {buttonContent}
       </div>
-      <button onClick={refresh}>refresh</button>
+      <div onClick={refresh} className='cursor-pointer col-end-13 text-right'>
+        <RefreshIcon className='h-7 w-7 mr-1.5 fill-slate-800' aria-hidden='true' />
+      </div>
     </div>
   );
 });
